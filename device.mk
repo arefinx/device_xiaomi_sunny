3a2c2912beea4e0de6077801099ecd55b14b44f4
blurzyy <selster02@gmail.com>
2023-01-14 14:47:54 +0800

    Revert "sm6150-common: Don't inherit updatable_apex.mk"
    - This reverts commit eec13c1391cc7b5dd2e04ac0cd6adcf93796123f as Updatable APEXes are a necessity to boot in U as Flattened APEXes are deprecated.
    
    Change-Id: Ib6926d8d33069e641db08931abeaacb92e7d70d7
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Include dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit virtual_ab_ota product
 $(call inherit-product, \
     $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
