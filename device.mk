e3097d01a0f233621d836967f4aa6665ccfe9b12
xenxynon <flynryder427@gmail.com>
2023-01-18 14:01:00 +0300

    mojito: Drop mock hidl power stats
    - E android.hardware.power.stats@1.0-service-mock Failed to getEnergyData

--- device.mk
+++ device.mk
@@ -329,7 +329,6 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor
