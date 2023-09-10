2c86c2748728a4cdeb6cf0375e48fb36ef338d9a
Ahmed Arefin <ahmedarefinsajjad@gmail.com>
2023-09-10 23:32:50 +0600

    sunny: Add RemovePackages
    - For apps that are conflicting with Dolby Atmos
    
    Signed-off-by: Ahmed Arefin <ahmedarefinsajjad@gmail.com>

--- device.mk
+++ device.mk
@@ -389,6 +389,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
