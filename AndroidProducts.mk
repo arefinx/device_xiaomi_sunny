d361cca83624007390b67d240bb4a04c001ef560
Ahmed Arefin <ahmedarefinsajjad@gmail.com>
2023-09-24 17:49:42 +0600

    sunny: Reorganize device tree
    - reorganized device tree according to lineageos
    
    Signed-off-by: Ahmed Arefin <ahmedarefinsajjad@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The PixelExperience Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/sunny.mk
+    $(LOCAL_DIR)/lineage_sunny.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_sunny-user \
+    lineage_sunny-userdebug \
+    lineage_sunny-eng
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -15,7 +15,7 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -25,13 +25,13 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$ANDROID_ROOT"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Warning headers and guards
 write_headers
 
 # The standard blobs
-write_makefiles "${MY_DIR}/../../proprietary-files.txt" true
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers
