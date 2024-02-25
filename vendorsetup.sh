8bfb2b78490f7127d0d2e7adbd3abb817789c0b8
Ahmed Arefin <ahmedarefinsajjad@gmail.com>
2024-02-25 23:37:24 +0600

    sunny: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Clone vendor and kernel trees
-git clone https://github.com/arefinx/vendor_xiaomi_sunny.git --depth=1
-git clone https://github.com/arefinx/kernel_xiaomi_sunny.git --depth=1
+git clone https://github.com/arefinx/vendor_xiaomi_sunny.git vendor/xiaomi/sunny --depth=1
+git clone https://github.com/arefinx/kernel_xiaomi_sunny.git kernel/xiaomi/sunny --depth=1
